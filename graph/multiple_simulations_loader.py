20cbdd0ba4ece8e09459e20ab47030c99d154a93
JudahGazit <gazitrotem@gmail.com>
2020-11-11 15:09:42 +0200

    showing graph in multiple optimization loading

--- graph/multiple_simulations_loader.py
+++ graph/multiple_simulations_loader.py
@@ -10,6 +10,13 @@ SimulationOptions = namedtuple('SimulationOptions',
                                ['cost_type', 'nodes', 'edges', 'wiring_factor', 'routing_factor', 'fuel_factor'])
 
 
+def cost(metrics, wiring_factor, routing_factor, fuel_factor):
+    wiring = metrics['wiring-cost']['normalized_value']
+    routing = metrics['routing-cost']['normalized_value']
+    fuel = metrics['fuel-cost']['normalized_value']
+    return float(wiring_factor) * wiring + float(routing_factor) * routing + float(fuel_factor) * fuel
+
+
 class MultipleOptimizationsLoader:
     def _union_metrics(self, metrics):
         unioned_metrics = {}
@@ -17,6 +24,9 @@ class MultipleOptimizationsLoader:
             metric_df = pd.DataFrame([m[metric] for m in metrics])
             unioned_metrics[metric] = metric_df.mean().to_dict()
         return unioned_metrics
+
+    def _best_metric(self, metrics, wiring_factor, routing_factor, fuel_factor):
+        return max(metrics, key=lambda m: cost(m, wiring_factor, routing_factor, fuel_factor))
 
     def _union_charts(self, charts):
         unioned_charts = {}
@@ -31,6 +41,16 @@ class MultipleOptimizationsLoader:
             }
         return unioned_charts
 
+    def _best_chart(self, charts, metrics, wiring_factor, routing_factor, fuel_factor):
+        charts_and_metrics = zip(charts, metrics)
+        best_chart = max(charts_and_metrics, key=lambda t: cost(t[1], wiring_factor, routing_factor, fuel_factor))[0]
+        return best_chart
+
+    def _best_graph(self, graphs, metrics, wiring_factor, routing_factor, fuel_factor):
+        graphs_and_metrics = zip(graphs, metrics)
+        best_graph = max(graphs_and_metrics, key=lambda t: cost(t[1], wiring_factor, routing_factor, fuel_factor))[0]
+        return best_graph
+
     @property
     def options(self):
         options = glob('optimize_results/*/*_*/*_*_*')
@@ -38,15 +58,26 @@ class MultipleOptimizationsLoader:
         options = [SimulationOptions(*option) for option in options]
         return options
 
-    def load(self, cost_type, nodes, edges, wiring_factor, routing_factor, fuel_factor):
-        files = glob(f'optimize_results/{cost_type}/{nodes}_{edges}/{wiring_factor}_{routing_factor}_{fuel_factor}/*.json')
+    def _format_results(self, strategy, metrics, charts, edges, *factors):
+        result = {
+            'edges': self._best_graph(edges, metrics, *factors)
+        }
+        if strategy == 'mean':
+            result['metric'] = self._union_metrics(metrics)
+            result['chart'] = self._union_charts(charts)
+        elif strategy == 'best':
+            result['metric'] = self._best_metric(metrics, *factors)
+            result['chart'] = self._best_chart(charts, metrics, *factors)
+        return result
+
+    def load(self, strategy, cost_type, nodes, edges, wiring_factor, routing_factor, fuel_factor):
+        files = glob(
+            f'optimize_results/{cost_type}/{nodes}_{edges}/{wiring_factor}_{routing_factor}_{fuel_factor}/*.json')
         data = [json.loads(open(filename).read()) for filename in files]
         metrics = [d['metrics'] for d in data]
         charts = [d['chart'] for d in data]
-        return {
-            'metric': self._union_metrics(metrics),
-            'chart': self._union_charts(charts)
-        }
+        edges = [d.get('edges', []) for d in data]
+        return self._format_results(strategy, metrics, charts, edges, wiring_factor, routing_factor, fuel_factor)
 
 
 if __name__ == '__main__':
--- ui/multiple_optimizations.py
+++ ui/multiple_optimizations.py
@@ -1,27 +1,44 @@
 from graph.multiple_simulations_loader import MultipleOptimizationsLoader
 from ui.utils import *
-import re
-# loader = MultipleOptimizationsLoader()
-#     num_nodes = request.args.get('nodes')
-#     num_edges = request.args.get('edges')
-#     wiring_factor = request.args.get('wiring')
-#     routing_factor = request.args.get('routing')
-#     fuel_factor = request.args.get('fuel')
-#     if num_nodes:
-#         data = loader.load(num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor)
-#     else:
-#         data = loader.options
-#     return jsonify(data)
+
+
+def _get_parameters(loader):
+    nodes_edges = {}
+    for option in loader.options:
+        key = option.cost_type, int(option.nodes), int(option.edges)
+        value = (option.wiring_factor, option.routing_factor, option.fuel_factor)
+        nodes_edges[key] = nodes_edges.get(key, []) + [value]
+    selected_nodes_edges = st.sidebar.radio('(Topology, Nodes, Edges)', sorted(nodes_edges),
+                                            format_func=lambda v: ', '.join(map(str, v)))
+    selected_params = st.sidebar.radio('(Wiring, Routing, Fuel)',
+                                       sorted(nodes_edges[selected_nodes_edges], reverse=True),
+                                       format_func=lambda v: ', '.join(map(lambda s: str(round(float(s), 3)), v)))
+    selected_option = selected_nodes_edges + selected_params
+    strategy = st.sidebar.select_slider('Strategy', ['best', 'mean'])
+    return selected_option, strategy
+
+
+def _recreate_graph(results):
+    graph = nx.Graph()
+    graph.add_weighted_edges_from([(r['source'], r['target'], r['weight']) for r in results['edges']])
+    return graph
+
+
+def _display_graph(graph, topology):
+    if topology == 'circular':
+        display_graph_in_circle(graph)
+    elif topology == 'lattice':
+        display_graph_as_lattice(graph)
 
 
 def multiple_optimizations():
     loader = MultipleOptimizationsLoader()
-    st.sidebar.text('(Topology, Nodes, Edges, Wiring, Routing, Fuel)')
-    selected_option = st.sidebar.radio('Parameters', options=[
-        f'({o.cost_type}, {o.nodes}, {o.edges}, {o.wiring_factor}, {o.routing_factor}, {o.fuel_factor})'
-        for o in loader.options
-    ])
-    selected_option = selected_option[1:-1]
-    results = loader.load(*selected_option.split(', '))
+    selected_option, strategy = _get_parameters(loader)
+    results = loader.load(strategy, *selected_option)
+    graph = _recreate_graph(results)
+    _display_graph(graph, selected_option[0])
     display_metrics(results['metric'])
-    display_chart_with_mean_line(results['chart'])
+    if strategy == 'best':
+        display_chart(results['chart'])
+    else:
+        display_chart_with_mean_line(results['chart'])
