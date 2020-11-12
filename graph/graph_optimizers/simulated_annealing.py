b885daa855e8c9354ccf1f85b6cb307a3ea8848f
JudahGazit <gazitrotem@gmail.com>
2020-11-12 09:38:37 +0200

    Fixed simulated_annealing.py

--- graph/graph_optimizers/simulated_annealing.py
+++ graph/graph_optimizers/simulated_annealing.py
@@ -1,3 +1,8 @@
+import copy
+import threading
+from time import sleep
+
+import numpy as np
 import random
 import signal
 
@@ -8,38 +13,56 @@ from graph.graph_optimizers.graph_optimizer_base import GraphOptimizerBase
 
 signal.signal = lambda *args, **kwargs: None
 
+def create_state(matrix):
+        return [
+            matrix,
+            [[a, b] for a, b in np.argwhere(matrix == 1).tolist() if a < b],
+            [[a, b] for a, b in np.argwhere(matrix == 0).tolist() if a < b],
+        ]
 
 class _GraphAnnealer(Annealer):
-    copy_strategy = 'slice'
-    Tmax = 30
-    Tmin = 1
+    Tmax = 1
+    Tmin = 1e-15
+    steps = 10000
+    updates = 200
 
-    def __init__(self, graph_cost: GraphCost, state, *args, **kwargs):
-        super().__init__(state, *args, **kwargs)
+    def copy_state(self, state):
+        matrix, non_zero_indices, zero_indices = state
+        return [np.copy(matrix), copy.copy(non_zero_indices), copy.copy(zero_indices)]
+
+    def __init__(self, graph_cost: GraphCost, matrix, *args, **kwargs):
+        super().__init__(create_state(matrix))
         self.graph_cost = graph_cost
-        self.len_state = len(state)
-        self.state = self.graph_cost.triangular_to_mat(state)
+
+    def random_change_edge(self):
+        a_index = random.randrange(len(self.state[1]))
+        b_index = random.randrange(len(self.state[2]))
+        a_index = self.state[1].pop(a_index)
+        b_index = self.state[2].pop(b_index)
+        self.state[1].append(b_index), self.state[2].append(a_index)
+        self.state[0][a_index[0], a_index[1]] = 0
+        self.state[0][a_index[1], a_index[0]] = 0
+        self.state[0][b_index[0], b_index[1]] = 1
+        self.state[0][b_index[1], b_index[0]] = 1
 
     def move(self):
-        a = random.randint(0, self.len_state - 1)
-        b = random.randint(0, self.len_state - 1)
-        a_index = self.graph_cost.triangular_index(a)
-        b_index = self.graph_cost.triangular_index(b)
-        a_value, b_value = self.state[a_index], self.state[b_index]
-        self.state[a_index[0], a_index[1]] = b_value
-        self.state[a_index[1], a_index[0]] = b_value
-        self.state[b_index[0], b_index[1]] = a_value
-        self.state[b_index[1], b_index[0]] = a_value
+        self.random_change_edge()
+
 
     def energy(self):
-        return self.graph_cost.cost(self.state)
+        return self.graph_cost.cost(self.state[0])
 
 
 class SimulatedAnnealing(GraphOptimizerBase):
     def _optimal_matrix(self):
         initial_edges_vec = self._randomize_edges()
-        annealer = _GraphAnnealer(self.graph_cost, initial_edges_vec)
-        auto_schedule = annealer.auto(1)
-        annealer.set_schedule(auto_schedule)
+        initial_state = self.graph_cost.triangular_to_mat(initial_edges_vec)
+        annealer = _GraphAnnealer(self.graph_cost, initial_state)
+        def kill():
+            sleep(120)
+            annealer.set_user_exit(None, None)
+        thread = threading.Thread(target=kill)
+        thread.start()
         res, cost = annealer.anneal()
+        res = res[0]
         return res
