e2e30ace0a4a58aa1dea77f763f7a426f17c3d78
JudahGazit <gazitrotem@gmail.com>
2020-11-18 13:32:02 +0200

    bugfix in metric_result.py

--- graph/metric_result.py
+++ graph/metric_result.py
@@ -4,8 +4,8 @@ import math
 class MetricResult:
     def __init__(self, value, normalization_factor=1, normalized_value=None):
         self.value = value
-        self.normalization_factor = normalization_factor or self.value * normalized_value
-        self.normalized_value = normalized_value or self.__calculate_normalized()
+        self.normalization_factor = normalization_factor if normalization_factor is not None else self.value * normalized_value
+        self.normalized_value = normalized_value if normalized_value is not None else self.__calculate_normalized()
 
     def __calculate_normalized(self):
         if math.isinf(self.value):
