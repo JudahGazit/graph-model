49e50796040a34bdb5280e9595889f080efee539
JudahGazit <gazitrotem@gmail.com>
2020-11-18 13:58:01 +0200

    refactored to use target values instead of total cost

--- graph/graph_optimizers/simulated_annealing.py
+++ graph/graph_optimizers/simulated_annealing.py
@@ -1,11 +1,10 @@
 import copy
-import threading
-from time import sleep
-
-import numpy as np
 import random
 import signal
 
+import numpy as np
+import pandas as pd
+from scipy.spatial.distance import euclidean
 from simanneal import Annealer
 
 from graph.graph_optimizers.graph_cost import GraphCost
@@ -13,17 +12,18 @@ from graph.graph_optimizers.graph_optimizer_base import GraphOptimizerBase
 
 signal.signal = lambda *args, **kwargs: None
 
+
 def create_state(matrix):
-        return [
-            matrix,
-            [[a, b] for a, b in np.argwhere(matrix == 1).tolist() if a < b],
-            [[a, b] for a, b in np.argwhere(matrix == 0).tolist() if a < b],
-        ]
+    return [
+        matrix,
+        [[a, b] for a, b in np.argwhere(matrix == 1).tolist() if a < b],
+        [[a, b] for a, b in np.argwhere(matrix == 0).tolist() if a < b],
+    ]
 
 class _GraphAnnealer(Annealer):
     Tmax = 1
     Tmin = 1e-15
-    steps = 25000
+    steps = 4000
     updates = 200
 
     def copy_state(self, state):
@@ -48,10 +48,21 @@ class _GraphAnnealer(Annealer):
     def move(self):
         self.random_change_edge()
 
-
     def energy(self):
-        cost = self.graph_cost.cost(self.state[0])
-        return cost
+        matrix = np.multiply(self.graph_cost.distance_matrix, self.state[0])
+        metrics = self.graph_cost.create_graph_metrics(matrix)
+        wiring_cost = metrics.wiring_cost().normalized_value
+        routing_cost = metrics.routing_cost().normalized_value
+        fuel_cost = metrics.fuel_cost().normalized_value
+        score = 0
+        for cost, goal in zip([wiring_cost, routing_cost, fuel_cost],
+                              [self.graph_cost.wiring_factor, self.graph_cost.routing_factor,
+                               self.graph_cost.fuel_factor]):
+            if goal is not None:
+                score += (1000 * (cost - goal)) ** 2
+        if score < 1e-1:
+            self.user_exit = True
+        return score
 
 
 class SimulatedAnnealing(GraphOptimizerBase):
--- ui/optimize.py
+++ ui/optimize.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from graph.graph_formatter import GraphFormatter
 from graph.graph_optimizer import GraphOptimizer
 from ui.utils import *
@@ -5,19 +7,24 @@ from ui.utils import *
 last_parameters = None
 current_graph = None
 
+OptimizeParameters = namedtuple('OptimizedParameters',
+                                ['num_nodes', 'num_edges', 'wiring_factor', 'routing_factor', 'fuel_factor',
+                                 'method', 'cost_type', 'optimizer'])
+
 
 def _get_parameters():
     cost_type = st.sidebar.selectbox('Network Type', ['circular', 'lattice'])
     num_nodes = st.sidebar.slider('Number of Nodes', 10, 300) if cost_type == 'circular' else st.sidebar.select_slider(
         'Number of Nodes', [i ** 2 for i in range(2, 20)], 16)
     mean_degree = st.sidebar.slider('Mean Degree', 1.5, 40.0, 2.0, step=0.01)
-    wiring_factor = st.sidebar.slider('Wiring Factor', -1.0, 1.0, 0.0, step=0.01)
-    routing_factor = st.sidebar.slider('Routing Factor', -1.0, 1.0, 0.0, step=0.01)
-    fuel_factor = st.sidebar.slider('Fuel Factor', -1.0, 1.0, 0.0, step=0.01)
+    wiring_factor = st.sidebar.slider('Target Wiring', 0.0, 1.0, 0.0, step=0.01) if st.sidebar.checkbox('Wiring?', True) else None
+    routing_factor = st.sidebar.slider('Target Routing', 0.0, 1.0, 0.0, step=0.01) if st.sidebar.checkbox('Routing?', True) else None
+    fuel_factor = st.sidebar.slider('Target Fuel', 0.0, 1.0, 0.0, step=0.01) if st.sidebar.checkbox('Fuel?', True) else None
     method = st.sidebar.select_slider('Method', ['minimize', 'maximize'], 'maximize')
     optimizer = st.sidebar.select_slider('Optimizer', ['annealing', 'genetic'])
     num_edges = int(num_nodes * mean_degree / 2)
-    return num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type, optimizer
+    return OptimizeParameters(num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type,
+                              optimizer)
 
 
 def _display_graph_by_cost_type(graph, cost_type):
@@ -33,29 +40,34 @@ def _initial_graph(num_nodes, cost_type):
     if cost_type == 'lattice':
         return nx.grid_2d_graph(int(math.sqrt(num_nodes)), int(math.sqrt(num_nodes)))
 
+
 # @st.cache
-def optimize_graph(num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type, optimizer_type):
-    optimizer = GraphOptimizer(num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type,
-                               optimizer_type)
+def optimize_graph(optimize_parameters: OptimizeParameters):
+    optimizer = GraphOptimizer(optimize_parameters.num_nodes,
+                               optimize_parameters.num_edges,
+                               optimize_parameters.wiring_factor,
+                               optimize_parameters.routing_factor,
+                               optimize_parameters.fuel_factor,
+                               optimize_parameters.method,
+                               optimize_parameters.cost_type,
+                               optimize_parameters.optimizer)
     return optimizer.optimize()
 
 
 def optimize():
     global last_parameters, current_graph
-    num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type, optimizer_type = _get_parameters()
+    optimize_parameters = _get_parameters()
     start_simulation = st.sidebar.button('Start Optimization (might take a while)')
 
     if start_simulation:
-        current_graph = optimize_graph(num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method,
-                                       cost_type, optimizer_type)
-        last_parameters = (
-        num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type, optimizer_type)
+        current_graph = optimize_graph(optimize_parameters)
+        last_parameters = optimize_parameters
 
-    if last_parameters == (
-    num_nodes, num_edges, wiring_factor, routing_factor, fuel_factor, method, cost_type, optimizer_type):
-        formatter = GraphFormatter(current_graph, topology=cost_type)
-        _display_graph_by_cost_type(current_graph.graph, cost_type)
+    if last_parameters == optimize_parameters:
+        formatter = GraphFormatter(current_graph, topology=optimize_parameters.cost_type)
+        _display_graph_by_cost_type(current_graph.graph, optimize_parameters.cost_type)
         display_metrics(formatter.format_metrics())
         display_charts(formatter.format_chart())
     else:
-        _display_graph_by_cost_type(_initial_graph(num_nodes, cost_type), cost_type)
+        _display_graph_by_cost_type(_initial_graph(optimize_parameters.num_nodes, optimize_parameters.cost_type),
+                                    optimize_parameters.cost_type)
