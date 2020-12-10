c4d58cbe827ababf6210eb8332b41e6102db09f7
JudahGazit <gazitrotem@gmail.com>
2020-12-10 14:18:42 +0200

    fixed major bug in CostBoundaries

--- graph/graph_metrics.py
+++ graph/graph_metrics.py
@@ -23,11 +23,11 @@ def manhatten(u, v, n):
     return cityblock(u, v)
 
 
-@dataclass
 class CostBoundaries:
-    wiring: MetricBoundaries = MetricBoundaries()
-    routing: MetricBoundaries = MetricBoundaries()
-    fuel: MetricBoundaries = MetricBoundaries()
+    def __init__(self, wiring=None, routing=None, fuel=None):
+        self.wiring = wiring or MetricBoundaries()
+        self.routing = routing or MetricBoundaries()
+        self.fuel = fuel or MetricBoundaries()
 
 class GraphMetrics:
     def __init__(self, graph_dataset=None, matrix=None, cost_boundaries=None):
@@ -60,21 +60,29 @@ class GraphMetrics:
         return gb
 
     def __create_cost_boundaries(self, cost_boundaries):
-        cost_boundaries = cost_boundaries or CostBoundaries()
-        cost_boundaries.wiring.optimal_value = self.__wiring_cost_bounds(optimal=True)
-        cost_boundaries.wiring.worst_value = self.__wiring_cost_bounds(optimal=False)
-        cost_boundaries.routing.optimal_value = self.__optimal_routing_cost()
-        cost_boundaries.fuel.optimal_value = self.__optimal_fuel_cost()
+        cost_boundaries = cost_boundaries if cost_boundaries is not None else CostBoundaries()
+        if cost_boundaries.wiring.optimal_value is None:
+            cost_boundaries.wiring.optimal_value = self.__wiring_cost_bounds(optimal=True)
+        if cost_boundaries.wiring.worst_value is None:
+            cost_boundaries.wiring.worst_value = self.__wiring_cost_bounds(optimal=False)
+        if cost_boundaries.routing.optimal_value is None:
+            cost_boundaries.routing.optimal_value = self.__optimal_routing_cost()
+        if cost_boundaries.fuel.optimal_value is None:
+            cost_boundaries.fuel.optimal_value = self.__optimal_fuel_cost()
         return cost_boundaries
 
     def __wiring_cost_bounds(self, optimal=True):
         total_number_of_possible_edges = self.number_of_nodes * (self.number_of_nodes - 1) / 2
-        percentile = self.number_of_edges / total_number_of_possible_edges
-        random_nodes = random.sample(range(self.number_of_nodes), min([200, self.number_of_nodes]))
-        distances = sorted([self.distances(u, v) for u, v in itertools.combinations(random_nodes, 2)], reverse=not optimal)
-        number_of_random_edges = len(distances)
-        sum_of_percentile_samples = sum(distances[:(max([int(number_of_random_edges * percentile), 1]))])
-        return sum_of_percentile_samples * (total_number_of_possible_edges / number_of_random_edges)
+        if self.number_of_nodes > 200:
+            percentile = self.number_of_edges / total_number_of_possible_edges
+            random_nodes = random.sample(range(self.number_of_nodes), min([200, self.number_of_nodes]))
+            distances = sorted([self.distances(u, v) for u, v in itertools.combinations(random_nodes, 2)], reverse=not optimal)
+            number_of_random_edges = len(distances)
+            sum_of_percentile_samples = sum(distances[:max([int(number_of_random_edges * percentile), 1])])
+            return sum_of_percentile_samples * (total_number_of_possible_edges / number_of_random_edges)
+        else:
+            distances = sorted([self.distances(u, v) for u, v in itertools.combinations(range(self.number_of_nodes), 2)], reverse=not optimal)
+            return sum(distances[:min([self.number_of_edges, len(distances)])])
 
     def __optimal_routing_cost(self):
         number_of_pairs = self.number_of_nodes * (self.number_of_nodes - 1) / 2
